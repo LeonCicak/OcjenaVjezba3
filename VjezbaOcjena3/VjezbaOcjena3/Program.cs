f4130faeffcd9de95b428e46d8e0dba456664b96
LeonCicak <leoncicak07@gmail.com>
2024-10-28 12:44:40 +0100

    Add files via upload

---
+++ VjezbaOcjena3/VjezbaOcjena3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbaOcjena3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite rečenicu: ");
+            string recenica = Console.ReadLine();
+
+
+            string[] rijec = recenica.Split(' ');
+
+
+            if (rijec.Length > 0)
+            {
+
+                string prvaRijec = rijec[0];
+
+                string zadnjaRijec = rijec[rijec.Length - 1];
+
+
+                Console.WriteLine("Prva riječ: " + prvaRijec);
+                Console.WriteLine("Zadnja riječ: " + zadnjaRijec);
+            }
+            else
+            {
+                Console.WriteLine("Niste unijeli točnu rečenicu");
+            }
+            Console.ReadKey();
+        }
+    }
+}
